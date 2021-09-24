ef8660464c88ecc04ea3f6d76200b7b2b990122d
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-09-25 02:38:15 +0700

    [Add] GET allColors

--- src/main/java/sit/int222/mongkolthorn/controllers/ColorsController.java
+++ src/main/java/sit/int222/mongkolthorn/controllers/ColorsController.java
@@ -1,4 +1,22 @@
 package sit.int222.mongkolthorn.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sit.int222.mongkolthorn.models.Colors;
+import sit.int222.mongkolthorn.repositories.ColorsRepository;
+
+import java.awt.*;
+import java.util.List;
+
+@RestController
 public class ColorsController {
+    @Autowired
+    private ColorsRepository colorsRepository;
+
+    @GetMapping("/allColors")
+    public List<Colors> colors() {
+        return colorsRepository.findAll();
+    }
+
 }
