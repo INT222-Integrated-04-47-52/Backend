d8cfeca31142b355abf7d03cac5c45c2e8c40287
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-09-24 12:02:54 +0700

    [Add] Storage management

--- src/main/java/sit/int222/mongkolthorn/services/StorageService.java
+++ src/main/java/sit/int222/mongkolthorn/services/StorageService.java
@@ -1,15 +1,17 @@
 package sit.int222.mongkolthorn.services;
 
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
+@Service
 public interface StorageService {
     void init();
 
-    String store(MultipartFile image, Long icecreamId);
+    String store(MultipartFile image, Long productId);
 
     Stream<Path> loadAll();
 
