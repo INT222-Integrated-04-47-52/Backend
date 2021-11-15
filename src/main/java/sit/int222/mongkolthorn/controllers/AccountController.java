abc77eb6d1a6569325c4a8a6ebb1418efb1fa26b
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-11-16 04:45:52 +0700

    [Update] account endpoint

--- src/main/java/sit/int222/mongkolthorn/controllers/AccountController.java
+++ src/main/java/sit/int222/mongkolthorn/controllers/AccountController.java
@@ -1,8 +1,12 @@
 package sit.int222.mongkolthorn.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import sit.int222.mongkolthorn.exceptions.ExceptionResponse;
+import sit.int222.mongkolthorn.exceptions.ProductException;
 import sit.int222.mongkolthorn.models.Account;
 import sit.int222.mongkolthorn.repositories.AccountRepository;
 
@@ -16,5 +20,24 @@ public class AccountController {
     @GetMapping("/allAccounts")
     public List<Account> account() {
         return accountRepository.findAll();
+    }
+
+    @GetMapping("/account/{account_id}")
+    public Account showAccountId(@PathVariable Long account_id) {
+        Account account = accountRepository.findById(account_id).orElse(null);
+        if (account == null) {
+            throw new ProductException(ExceptionResponse.ERROR_CODE.ACCOUNT_ID_NOT_EXIST,
+                    "Account id: " + account_id + " does not exist.");
+        } else return accountRepository.getOne(account_id);
+    }
+
+    @DeleteMapping("/delete/{account_id}")
+    public void deleteProduct(@PathVariable Long account_id) {
+        Account account = accountRepository.findById(account_id).orElse(null);
+        if (account == null) {
+            throw new ProductException(ExceptionResponse.ERROR_CODE.ACCOUNT_ID_NOT_EXIST,
+                    "Can't delete. Account id: " + account_id + " does not exist.");
+        } else
+        accountRepository.deleteById(account_id);
     }
 }
--- src/main/java/sit/int222/mongkolthorn/controllers/LoginController.java
+++ src/main/java/sit/int222/mongkolthorn/controllers/LoginController.java
@@ -1,8 +1,12 @@
 package sit.int222.mongkolthorn.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import sit.int222.mongkolthorn.exceptions.ExceptionResponse;
+import sit.int222.mongkolthorn.exceptions.ProductException;
 import sit.int222.mongkolthorn.models.Login;
 import sit.int222.mongkolthorn.repositories.LoginRepository;
 
@@ -16,5 +20,24 @@ public class LoginController {
     @GetMapping("/login")
     public List<Login> login() {
         return loginRepository.findAll();
+    }
+
+    @GetMapping("/login/{login_id}")
+    public Login showLoginId(@PathVariable Long login_id) {
+        Login login = loginRepository.findById(login_id).orElse(null);
+        if (login == null) {
+            throw new ProductException(ExceptionResponse.ERROR_CODE.ACCOUNT_ID_NOT_EXIST,
+                    "Login id: " + login_id + " does not exist.");
+        } else return loginRepository.getOne(login_id);
+    }
+
+    @DeleteMapping("/delete/{login_id}")
+    public void deleteProduct(@PathVariable Long login_id) {
+        Login login = loginRepository.findById(login_id).orElse(null);
+        if (login == null) {
+            throw new ProductException(ExceptionResponse.ERROR_CODE.LOGIN_ID_NOT_EXIST,
+                    "Can't delete. Login id: " + login_id + " does not exist.");
+        } else
+            loginRepository.deleteById(login_id);
     }
 }
