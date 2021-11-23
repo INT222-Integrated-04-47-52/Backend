1ca310fe8e9ad29ba7e228480251fe4fb5c64008
Noochajee Phonbooncharoenchai <noochajee.nn@mail.kmutt.ac.th>
2021-11-24 06:02:47 +0700

    [Add] config

--- src/main/java/sit/int222/mongkolthorn/config/WebSecurityConfig.java
+++ src/main/java/sit/int222/mongkolthorn/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package sit.int222.mongkolthorn.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,7 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import sit.int222.mongkolthorn.services.TokenService;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,6 +23,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public WebSecurityConfig(TokenService tokenService) {
         this.tokenService = tokenService;
     }
+
+    @Value("#{'${mongkolthorn.origin.method}'.split(',')}")
+    private String[] methodList;
+    @Value("#{'${mongkolthorn.origin.host}'.split(',')}")
+    private String[] hostList;
+    @Value("#{'${mongkolthorn.origin.header}'.split(',')}")
+    private String[] headerList;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -31,11 +43,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable().csrf().disable()
+        http.cors(
+                config -> {
+                    CorsConfiguration cors = new CorsConfiguration();
+                    cors.setAllowCredentials(true);
+                    cors.setAllowedOrigins(Arrays.asList(hostList));
+                    cors.setAllowedMethods(Arrays.asList(methodList));
+                    cors.setAllowedHeaders(Arrays.asList(headerList));
+
+                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                    source.registerCorsConfiguration("/**", cors);
+
+                    config.configurationSource(source);
+                }
+                ).csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/login").anonymous()
+                .antMatchers("/login","/images","/image/{imageName}").anonymous()
                 .antMatchers("/allProducts","/search","/filter/**").anonymous()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .antMatchers("/user/**").hasAuthority("USER")
